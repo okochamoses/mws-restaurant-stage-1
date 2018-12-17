d7e2420599cabd5892df7a3443a208ca867bc80b
Moses Okocha <Moses.Okocha@Interswitchng.com>
2018-12-17 13:55:11 +0100

    feat: change xhr requests to fetch requests

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,3 +1,21 @@
+const idbKeyVal = {
+  get(key) {
+    return dbPromise.then(db => {
+      return db
+        .transaction('restaurants')
+        .objectStore('restaurants')
+        .get(key);
+    });
+  },
+  set(key, val) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('restaurants', 'readwrite');
+      tx.objectStore('restaurants').put(val, key);
+      return tx.complete;
+    });
+  }
+};
+
 /**
  * Common database helper functions.
  */
@@ -8,27 +26,25 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(DBHelper.DATABASE_URL)
+    .then(response => response.json())
+    .then(function(jsonResponse) {
+      console.log("GC: fetch is working!");
+      callback(null, jsonResponse);
+    })
+    .catch(function(error) {
+      console.log("GC: Uh oh, fetch is bad");
+      const errorMessage = (`Request failed. Returned status of ${error}`);
+      callback(errorMessage, null);
+    });
   }
 
   /**
@@ -150,7 +166,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
