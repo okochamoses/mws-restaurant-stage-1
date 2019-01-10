f0bc2d6ae18037173540cf412c8a8a08b88ba31a
okochamoses <okocha.chukwutem@gmail.com>
2019-01-11 00:55:49 +0100

    Fix service worker and accessibility

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let staticCacheName = "restaurant-cache";
+let staticCacheName = "restaurant-cache-v2";
 let urlsToCache = [
   "./manifest.json",
   "./",
@@ -8,6 +8,7 @@ let urlsToCache = [
   "./js/main.js",
   "./js/restaurant_info.js",
   "./js/dbhelper.js",
+  "./js/idb.js",
   "./js/sw_registration.js",
   "./data/restaurants.json",
   "./img/1.jpg",
@@ -19,9 +20,16 @@ let urlsToCache = [
   "./img/7.jpg",
   "./img/8.jpg",
   "./img/9.jpg",
-  "./img/10.jpg"
+  "./img/10.jpg",
+  "./img/icons/icons-192.png",
+  "./img/icons/icons-512.png",
+  "./sw.js",
+  "http://localhost:1337/restaurants",
+  "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
+  "https://fonts.googleapis.com/css?family=Open+Sans:300,400"
 ];
-console.log("MMMMM");
+console.log("SW.js page");
 /**
  * Installation of service worker
  */
@@ -62,9 +70,36 @@ self.addEventListener("activate", function(event) {
  * Fetching for offline content viewing
  */
 self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  let requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+      // if (requestUrl.pathname.startsWith('/restaurant.html')) {
+        event.respondWith(serveRestaurantHTML(event.request));
+        return;
+      }
+      event.respondWith(
+        caches.match(event.request).then(function(response) {
+          console.log(event.request.url);
+          return response || fetch(event.request);
+        })
+      ); 
+    // }
 });
+
+  // serves restaurants.html page
+  function serveRestaurantHTML(request) {
+    console.log(request);
+    // Use this url to store & match retaurants.hmtl in the cache.
+    // This means you only store one copy of restaurants.html
+    const storageUrl = request.url.split('?')[0];
+
+    return caches.open(staticCacheName).then(function(cache) {
+      return cache.match(storageUrl).then(function(response) {
+        if (response) return response;
+
+        return fetch(request).then(function(networkResponse) {
+          cache.put(storageUrl, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    });
+  }
